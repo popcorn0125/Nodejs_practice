4bc5c6caabfc2166e62eb987b08ae1af8d729f3d
김준식 <105399710+popcorn0125@users.noreply.github.com>
2024-08-22 11:24:20 +0900

    초기세팅

--- Day02/app.js
+++ Day02/app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-app.get('/home', (req, res) => {
+app.get("/home", (req, res) => {
     req.app.render("home/Home", {}, (err, html) => {
         res.end(html);
     });
@@ -18,25 +18,46 @@ app.get('/profile', (req, res) => {
     req.app.render("profile/Profile", {}, (err, html) => {
         res.end(html);
     });
-})
+});
 
 app.get('/member', (req, res) => {
     req.app.render("member/Member", {}, (err, html) => {
         res.end(html);
     });
-})
+});
+
+app.get('/login', (req, res) => {
+    req.app.render("member/Login", {}, (err, html) => {
+        res.end(html);
+    });
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/member');
+});
+
+app.get('/joinus', (req, res) => {
+    req.app.render("member/Joinus", {}, (err, html) => {
+        res.end(html);
+    });
+});
+
+app.post('/joinus', (req, res) => {
+    // 회원 가입 처리 후 목록으로 갱신
+    res.redirect('/member');
+});
 
 app.get('/gallery', (req, res) => {
     req.app.render("gallery/Gallery", {}, (err, html) => {
         res.end(html);
     });
-})
+});
 
 app.get('/shop', (req, res) => {
     req.app.render("shop/Shop", {}, (err, html) => {
         res.end(html);
     });
-})
+});
 
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=> {
