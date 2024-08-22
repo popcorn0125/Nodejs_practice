8cd7f2addcda81a346addcfaa1f3e89a898445ca
김준식 <105399710+popcorn0125@users.noreply.github.com>
2024-08-22 14:12:10 +0900

    쿠키 및 error-handler 설정

--- Day02/app20240822-01.js
+++ Day02/app20240822-01.js
@@ -2,7 +2,6 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 app.set('port', 3000);
 app.set('views', 'views');
@@ -12,8 +11,6 @@ app.use(express.static("public"));
 // post 방식으로 파라미터 전달 받기 위한 설정
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-// 쿠키 사용 미들웨어 설정
-app.use(cookieParser());
 
 const memberList = [
     { no: 101, id:'user01', password: '1234', name:'홍길동', email: 'hong@gmail.com'},
@@ -23,45 +20,31 @@ const memberList = [
 ]
 let noCnt = 105;
 
-// 요청 라우팅 사용
-const router = express.Router();
-
-
-router.route('/home').get((req,res) => {
+app.get("/home", (req, res) => {
     req.app.render("home/Home", {}, (err, html) => {
         res.end(html);
     });
 })
 
-
-router.route('/profile').get((req, res) => {
+app.get('/profile', (req, res) => {
     req.app.render("profile/Profile", {}, (err, html) => {
         res.end(html);
     });
 });
 
-
-router.route('/member').get((req, res) => {
+app.get('/member', (req, res) => {
     req.app.render("member/Member", {}, (err, html) => {
         res.end(html);
     });
 });
 
-
-router.route('/login').get((req, res) => {
+app.get('/login', (req, res) => {
     req.app.render("member/Login", {}, (err, html) => {
-        // 사용자(접속자)의 로컬에 쿠키가 저장 된다.
-        res.cookie('user', {
-            id: 'Testuser',
-            name: '테스트 유저',
-            authorized: true
-        });
         res.end(html);
     });
 });
 
-
-router.route('/login').post((req, res) => {    
+app.post('/login', (req, res) => {
     console.log(req.body.id, req.body.password);
     const idx = memberList.findIndex(member => member.id === req.body.id);
     if(idx != -1){
@@ -77,58 +60,29 @@ router.route('/login').post((req, res) => {
     res.redirect('/member');
 });
 
-
-router.route('/joinus').get((req, res) => {
+app.get('/joinus', (req, res) => {
     req.app.render("member/Joinus", {}, (err, html) => {
         res.end(html);
     });
 });
 
-
-router.route('/joinus').post((req, res) => {
+app.post('/joinus', (req, res) => {
     // 회원 가입 처리 후 목록으로 갱신
     res.redirect('/member');
 });
 
-
-router.route('/gallery').get((req, res) => {
+app.get('/gallery', (req, res) => {
     req.app.render("gallery/Gallery", {}, (err, html) => {
         res.end(html);
     });
 });
 
-
-router.route('/shop').get((req, res) => {
+app.get('/shop', (req, res) => {
     req.app.render("shop/Shop", {}, (err, html) => {
         res.end(html);
     });
 });
 
-
-//router 설정 맨 아래에 미들웨어 등록 
-app.use('/', router);
-
-// 등록되지 않은 패스에 대해 페이지 오류 응답
-// app.all('*', function (req, res) {
-//     res.status(404).send('<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>')
-// });
-//오류 핸들러 모듈 사용
-const expressErrorHandler = require('express-error-handler');
-//모든 라우터 처리 후 404 오류 페이지 처리
-const errorHandler = expressErrorHandler({
-    static: {
-        '404': './public/404.html'
-    }
-});
-
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
-
-
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
-
-// 서버 실행
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=> {
     console.log(`Run on server >>> http://localhost:${app.get('port')}`);
